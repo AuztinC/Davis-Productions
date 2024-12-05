55195ae0445736a094b65640c4e7b7bd4837d83e
Austin Cripe <aaustinc@live.com>
2024-12-05 10:20:04 -0500

    Update CategoryContent.tsx

--- src/components/warehouse/CategoryContent.tsx
+++ src/components/warehouse/CategoryContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import '../../css/Banner.css'
 // import { format, parseISO} from 'date-fns';
 
